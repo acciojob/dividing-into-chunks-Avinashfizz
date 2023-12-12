e0816b3004ab85409b5c2e86fb39894847d67d2d
Avinashfizz <130842159+Avinashfizz@users.noreply.github.com>
2023-12-12 16:41:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,26 @@ const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
   // Write your code here
+   let A =[];
+	let Ans =[];
+	let i = 0;
+	let j = 0;
+	let sum = 0;
+	while(j<arr.length){
+		if(sum+arr[j]<=n){
+			A.push(arr[j]);
+			sum+=arr[j];
+			j++;
+		}else{
+			Ans[i]=A;
+			i++;
+			A =[];
+			A.push(arr[j]);
+			sum=arr[j];
+			j++;
+		}
+	}
+	console.log(ans);
 };
 
 const n = prompt("Enter n: ");
